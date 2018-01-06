adc42b4b8f97a9242868cf3de8b46c71b04dd0e2
Greg Kronmiller <kronmillerg@gmail.com>
2018-01-06 14:58:12 -0800

    Bring in logging code from WaRTS.

--- src/main.py
+++ src/main.py
@@ -3,9 +3,18 @@ import sys
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 
+from src.logconfig import newLogger
+
+log = newLogger(__name__)
+
 def main():
+    log.info("Begin.")
+
     app = MyApp()
     app.run()
+
+    # Not reached; I think Panda3D calls sys.exit when you close the window.
+    log.info("End.")
 
 class MyApp(ShowBase):
     def __init__(self):
