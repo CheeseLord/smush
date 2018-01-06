7dea08d5e3a39ed4195a6b2de8d60066a58a6edd
Greg Kronmiller <kronmillerg@gmail.com>
2018-01-06 13:24:41 -0800

    Add example environment.

--- src/main.py
+++ src/main.py
@@ -8,6 +8,15 @@ class MyApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
 
+        # Load the environment model.
+        self.scene = self.loader.loadModel("models/environment")
+        self.scene.reparentTo(self.render)
+
+        # Apply scale and position transforms on the model.
+        # Something something magic numbers bad something something.
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(-8, 42, 0)
+
 if __name__ == "__main__":
     main()
 
