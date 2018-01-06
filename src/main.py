c2bc4e92fb3888be264e4d8827bdcddcef7225cb
Greg Kronmiller <kronmillerg@gmail.com>
2018-01-06 13:16:35 -0800

    Super trivial Panda3D "hello world" program.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
-print "Ship it."
+from direct.showbase.ShowBase import ShowBase
+
+def main():
+    app = MyApp()
+    app.run()
+
+class MyApp(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+
+if __name__ == "__main__":
+    main()
+
